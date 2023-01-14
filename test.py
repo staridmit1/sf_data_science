bf04f20882852e75e40298cae575502b16b5965f
staridmit1 <d.starikov@list.ru>
2023-01-14 19:35:27 +0300

    new

--- test.py
+++ test.py
@@ -1,14 +1,2 @@
 x = 0
 y = 0
-while (compass := input()) != "СТОП":
-    steps = int(input())
-    if compass == "СЕВЕР":
-        y += steps
-    elif compass == "ЮГ":
-        y -= steps
-    elif compass == "ЗАПАД":
-        x -= steps
-    elif compass == "ВОСТОК":
-        x += steps
-print(y)
-print(x)
